709c3fcaf17d29c13eda2572972a48ed91109276
Lisha Singh <hellohellohellow16@gmail.com>
2024-03-27 15:11:42 +0530

    loading added

--- lib/src/pages/reports.dart
+++ lib/src/pages/reports.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:shopos/src/blocs/report/report_cubit.dart';
 import 'package:shopos/src/config/colors.dart';
@@ -93,9 +94,25 @@ class _ReportsPageState extends State<ReportsPage> {
           setState(() {
             _showLoader = false;
           });
+//Lottie
+          if(state is ReportLoading){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Scaffold(
+                  backgroundColor: Colors.white,// Adjust Scaffold as needed
+                  body: Center(
 
+                    child: Lottie.asset('assets/anims/report_loading.json', ), // Replace with your Lottie animation asset
+
+                  ),
+                ),
+              ),
+            );
+          }
           /// View
           if (state is ReportsView) {
+            Navigator.pop(context);
             _handleReportsView(state);
           }
         },
--- lib/src/pages/splash.dart
+++ lib/src/pages/splash.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,6 +10,7 @@ import 'package:shopos/src/pages/sign_in.dart';
 import 'package:shopos/src/provider/billing.dart';
 import 'package:shopos/src/services/LocalDatabase.dart';
 import 'package:shopos/src/services/api_v1.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SplashScreen extends StatefulWidget {
   BuildContext context;
@@ -17,13 +20,92 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
 class _SplashScreenState extends State<SplashScreen> {
+  late String _latestVersion = '';
+  late String _currentVersion = '1.0.1';
+
   ///
   @override
   void initState() {
     super.initState();
-    authStatus();
- //   getDataFromDatabase();
+    checkForUpdates();
+    // authStatus();
+    //   getDataFromDatabase();
+  }
+
+  Future<void> _launchURL() async {
+    final Uri _url = Uri.parse('https://bharatpos.xyz');
+
+    if (await canLaunchUrl(_url)) {
+      await launchUrl(_url, mode: LaunchMode.externalApplication);
+    } else {
+      throw Exception('Could not launch $_url');
+    }
+  }
+
+
+  Future<void> checkForUpdates() async {
+    try {
+      final response = await ApiV1Service.getRequest('/version/latest/bharatPos');
+
+      if (response.statusCode == 200) {
+        print("response.statuscode = ${response.statusCode}");
+
+        _latestVersion = response.data['data']['version'];
+        print("latestVersion: = $_latestVersion");
+        if (_currentVersion != _latestVersion) {
+          // downloadUpdate();
+          // _updateAvailable = true;
+          print("download");
+          alertUpdate();
+        } else {
+          authStatus();
+        }
+      } else {
+        print('Failed to fetch latest version. Error ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching latest version: $e');
+    }
+  }
+
+  Future<void> alertUpdate() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Update Available'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text('A new version of BharatPOS is available.'),
+                const Text('Please update to the latest version.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Update'),
+              onPressed: () {
+                _launchURL();
+                _startUpdateCheck();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _startUpdateCheck() {
+    // Start a timer to check for updates every 5 seconds
+    Timer.periodic(Duration(seconds: 3), (timer) {
+      if (_currentVersion == _latestVersion) {
+        authStatus();
+      }
+    });
   }
 
   Future<void> authStatus() async {
@@ -41,10 +123,8 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
-
   getDataFromDatabase() async {
-
-   /*// DatabaseHelper().DeleteDatabase();
+    /*// DatabaseHelper().DeleteDatabase();
     final provider = Provider.of<Billing>(
       widget.context,
     );
@@ -58,23 +138,25 @@ class _SplashScreenState extends State<SplashScreen> {
     });*/
   }
 
-
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       primary: false,
       appBar: AppBar(
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarIconBrightness: Brightness.dark,
-          statusBarColor: /*Color.fromARGB(255, 81, 163, 251)*/Colors.white,
+          statusBarColor: /*Color.fromARGB(255, 81, 163, 251)*/ Colors.white,
         ),
-        backgroundColor: /*Color.fromARGB(255, 81, 163, 251)*/Colors.white,
+        backgroundColor: /*Color.fromARGB(255, 81, 163, 251)*/ Colors.white,
       ),
-      backgroundColor: /*Color.fromARGB(255, 81, 163, 251)*/Colors.white,
+      backgroundColor: /*Color.fromARGB(255, 81, 163, 251)*/ Colors.white,
       body: Center(
         child: Container(
-          width: 700,
-          child: SvgPicture.asset("assets/icon/BharatPos.svg",fit: BoxFit.cover,)),
+            width: 700,
+            child: SvgPicture.asset(
+              "assets/icon/BharatPos.svg",
+              fit: BoxFit.cover,
+            )),
       ),
     );
   }
